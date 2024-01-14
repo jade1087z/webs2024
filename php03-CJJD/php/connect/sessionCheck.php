c210d6a9df3169aa48fa06551c6c0d3d9d45f35d
jade1087z <90414378+jade1087z@users.noreply.github.com>
2024-01-15 08:08:46 +0900

    12

--- php03-CJJD/php/connect/sessionCheck.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!isset($_SESSION['myMemberId'])) {
-    echo "<script>alert('로그인을 해주세요.');</script>";
-    echo '<script>window.location.href = "../login/login.php";</script>';
-}
-;
-?>
--- php03-CJJD/php/login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../connect/session.php";
-
-unset($_SESSION['myMemberId']);
-unset($_SESSION['youEmail']);
-unset($_SESSION['youName']);
-
-Header("Location: ../main/main.php");
-?>
